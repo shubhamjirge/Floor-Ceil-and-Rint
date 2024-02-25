1716b018e1fe4b142cefb09a5c87e12fd4e05ab5
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-02-25 13:06:14 +0530

    Add files via upload

---
+++ FloorCeilandRint.py
@@ -0,0 +1,15 @@
+"""
+Title     : Floor, Ceil and Rint
+Subdomain : Numpy
+Domain    : Python
+"""
+import numpy as np
+
+np.set_printoptions(legacy="1.13")
+A = np.array(input().split(), float)
+print(np.floor(A))
+print(np.ceil(A))
+print(np.rint(A))
+
+# Alternate solution
+# print(*(f(A) for f in [np.floor, np.ceil, np.rint]), sep='\n')
